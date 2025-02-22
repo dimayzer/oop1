6b547929104ad2fff76dccc3a27074c5bb1b11d6
dmxzr <99674344+dimayzer@users.noreply.github.com>
2025-02-22 18:20:48 +0300

    Create UI.hpp

---
+++ UI/UI.hpp
@@ -0,0 +1,29 @@
+#ifndef _UI_HPP_
+#define _UI_HPP_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+#define MAIN_MENU	"\n(1) Array to TOML\n"  \
+					"(2) TOML to Array\n"	\
+					"(3) Exit\n"
+
+
+
+#define TYPE_OF_STRING	"\n(1) default int\n"	\
+						"(2) const char*\n"		\
+						"(3) const char* + size_t\n"	\
+						"(4) std::string\n"
+
+
+#define TYPE_OF_TOML	"\n(1) const char*\n"		\
+						"(2) const char* + size_t\n"	\
+						"(3) std::string\n"
+
+
+
+#endif //_UI_HPP_
